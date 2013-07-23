0adb4d7d1bec3cd0e44f5bb7b67fef832b3af8d9
Yaroslav Gaponov <glygapon@avid.com>
2013-07-23 05:52:39 -0400

    daemon library changed

--- libs/utils/daemon.js
+++ libs/utils/daemon.js
@@ -6,17 +6,35 @@ var fs  = require("fs");
 var path = require("path");
 var cp = require("child_process");
 
+var PID = {
+    exists: function(pidfile, cb) {
+        fs.exists(pidfile, cb);
+    },
+    save: function(pid, pidfile, cb) {
+        fs.writeFile(pidfile, pid, cb);
+    },
+    load: function(pidfile, cb) {
+        fs.readFile(pidfile, function(err, data) {
+            if (err) return cb(err);
+            return cb(null, parseInt(data));
+        });
+    },
+    remove: function(pidfile, cb) {
+        fs.unlink(pidfile, cb);
+    }    
+};
+
 var Daemon = function(filename) {
     this._module = filename;
-    this._pidfile = path.dirname(filename) + "/" +  path.basename(filename, path.extname(filename)) + ".pid";
+    this._pid = path.dirname(filename) + "/" +  path.basename(filename, path.extname(filename)) + ".pid";
 }
 
 Daemon.prototype.start = function() {
     var self = this;
-    fs.exists(self._pidfile, function(found) {
+    PID.exists(self._pid, function(found) {
         if (!found)  {           
             var p = cp.fork(self._module);
-            Daemon.pid_save(p.pid, self._pidfile, function() {
+            PID.save(p.pid, self._pid, function() {
                 console.log(self._module + " daemon started.");
                 process.exit();
             });
@@ -29,12 +47,12 @@ Daemon.prototype.start = function() {
 
 Daemon.prototype.stop = function() {
     var self = this;
-    fs.exists(self._pidfile, function(found) {
+    PID.exists(self._pid, function(found) {
         if (found) { 
-            Daemon.pid_load(self._pidfile, function(err, pid) {
+            PID.load(self._pid, function(err, pid) {
                 if (!err && pid) {
                     process.kill(pid, 'SIGHUP');
-                    Daemon.pid_remove(self._pidfile,  function() {
+                    PID.remove(self._pid,  function() {
                         console.log(self._module + " daemon stopped.");
                         process.exit();
                     });
@@ -49,22 +67,6 @@ Daemon.prototype.stop = function() {
         }
     });
 }
-
-Daemon.pid_save = function(pid, pidfile, cb) {
-    fs.writeFile(pidfile, pid, cb);
-}
-
-Daemon.pid_load = function(pidfile, cb) {
-    fs.readFile(pidfile, function(err, data) {
-        if (err) return cb(err);
-        return cb(null, parseInt(data));
-    });
-}
-
-Daemon.pid_remove = function(pidfile, cb) {
-    fs.unlink(pidfile, cb);
-}
-
 
 module.exports = function(filename) {
     return new Daemon(filename);
